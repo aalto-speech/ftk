b22af6335529a34968baf282040c5df42cb085ac
Matti Varjokallio <matti.varjokallio@aalto.fi>
2013-01-31 13:45:23 +0200

    Implementation roughly there.

--- fe.cc
+++ fe.cc
@@ -1,3 +1,4 @@
+#include <limits>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -12,6 +13,7 @@ class Token {
     public:
         int source;
         double cost;
+        Token(): source(-1), cost(std::numeric_limits<double>::max()) {};
 };
 
 bool operator< (const Token& token1, const Token &token2)
@@ -48,10 +50,48 @@ int main(int argc, char* argv[]) {
     }
     vocabfile.close();
 
-    std::vector<Node> search;
+    std::vector<std::string> best_path;
     while (getline(std::cin, line)) {
-        search.resize(line.length());
+        std::vector<Node> search(line.length());
 
+        for (int i=0; i<line.length(); i++) {
+
+            // Possible factors starting from the beginning of the line
+            if (vocab.find(line.substr(0, i+1)) != vocab.end()) {
+                Token tok;
+                tok.cost = vocab[line.substr(0, i+1)];
+                search[i].push(tok);
+            }
+
+            // Factors originating from other positions
+            for (int j=0; j<i; j++) {
+                if (vocab.find(line.substr(j+1, i-j)) != vocab.end()) {
+                    Token tok;
+                    tok.source  = j;
+                    tok.cost = vocab[line.substr(j+1, i-j)];
+                    search[i].push(tok);
+                }
+            }
+        }
+
+        // Look up the best path
+        int target = search.size()-1;
+        Token top = search[target].top();
+        int source = top.source;
+        while (true) {
+            best_path.push_back(line.substr(source+1, target-source));
+            if (source == -1) break;
+            target = source;
+            Token top = search[target].top();
+            source = top.source;
+        }
+
+        // Print out the best path
+        for (int i=best_path.size()-1; i<0; i--)
+            std::cout << best_path[i] << " ";
+        std::cout << best_path[0] << std::endl;
+
+        best_path.clear();
     }
 
     exit(0);
