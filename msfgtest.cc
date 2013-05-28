a273dc87a2c6a69c80207a5677caa4343406ce41
Matti Varjokallio <matti.varjokallio@aalto.fi>
2013-05-28 14:56:23 +0300

    New test suite for multi string graphs.

---
+++ msfgtest.cc
@@ -0,0 +1,39 @@
+#include <map>
+#include <string>
+#include <vector>
+#include <cmath>
+
+#include "defs.hh"
+#include "msfgtest.hh"
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+using namespace std;
+
+
+CPPUNIT_TEST_SUITE_REGISTRATION (msfgtest);
+
+void msfgtest :: setUp (void)
+{
+}
+
+void msfgtest :: tearDown (void)
+{
+}
+
+void msfgtest :: MultiStringFactorGraphTest1 (void)
+{
+    CPPUNIT_ASSERT (false);
+}
+
+void msfgtest :: MultiStringFactorGraphTest2 (void)
+{
+    CPPUNIT_ASSERT (false);
+}
+
+// No possible segmentation
+void msfgtest :: MultiStringFactorGraphTest3 (void)
+{
+    CPPUNIT_ASSERT (false);
+}
---
+++ msfgtest.hh
@@ -0,0 +1,27 @@
+#ifndef MSFG_CPPUNIT
+#define MSFG_CPPUNIT
+
+#include "MSFG.hh"
+
+#include <cppunit/TestFixture.h>
+#include <cppunit/extensions/HelperMacros.h>
+
+class msfgtest : public CPPUNIT_NS :: TestFixture
+{
+    CPPUNIT_TEST_SUITE (msfgtest);
+    CPPUNIT_TEST (MultiStringFactorGraphTest1);
+    CPPUNIT_TEST (MultiStringFactorGraphTest2);
+    CPPUNIT_TEST (MultiStringFactorGraphTest3);
+    CPPUNIT_TEST_SUITE_END ();
+
+    public:
+        void setUp(void);
+        void tearDown(void);
+
+    protected:
+        void MultiStringFactorGraphTest1(void);
+        void MultiStringFactorGraphTest2(void);
+        void MultiStringFactorGraphTest3(void);
+};
+
+#endif
