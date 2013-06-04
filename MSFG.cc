ecea9a3ff1388d1e9075c6f1ac32943143f8c92b
Matti Varjokallio <matti.varjokallio@aalto.fi>
2013-06-04 17:37:08 +0300

    Use msfg_node_idx_t where appropriate.

--- MSFG.cc
+++ MSFG.cc
@@ -20,27 +20,27 @@ bool ascending_by_first(pair<int, int> i,pair<int, int> j) { return (i.first < j
 void
 MultiStringFactorGraph::add(const FactorGraph &text)
 {
-    map<int, int> created_nodes;
-    vector<int> connect_to_end_node; // MSFG indices to connect to end node
-    map<int, int> nodes_to_process; // Key is node idx in text FG, value idx in MSFG
+    map<fg_node_idx_t, msfg_node_idx_t> created_nodes;
+    vector<msfg_node_idx_t> connect_to_end_node; // MSFG indices to connect to end node
+    map<fg_node_idx_t, msfg_node_idx_t> nodes_to_process; // Key is node idx in text FG, value idx in MSFG
     nodes_to_process[0] = 0;
 
     while (nodes_to_process.size() > 0) {
 
-        int fg_src_node = nodes_to_process.begin()->first;
-        int msfg_src_node = nodes_to_process.begin()->second;
+        fg_node_idx_t fg_src_node = nodes_to_process.begin()->first;
+        msfg_node_idx_t msfg_src_node = nodes_to_process.begin()->second;
         nodes_to_process.erase(nodes_to_process.begin());
 
         const FactorGraph::Node &fg_node = text.nodes[fg_src_node];
         for (auto fg_arcit = fg_node.outgoing.cbegin(); fg_arcit != fg_node.outgoing.cend(); ++fg_arcit) {
 
-            int fg_target_node = (**fg_arcit).target_node;
+            fg_node_idx_t fg_target_node = (**fg_arcit).target_node;
             string target_node_factor = text.get_factor(fg_target_node);
             if (target_node_factor == start_end_symbol) {
                 connect_to_end_node.push_back(msfg_src_node);
                 continue;
             }
-            int msfg_target_node = -1;
+            msfg_node_idx_t msfg_target_node = 0;
 
             // Check existing target nodes
             MultiStringFactorGraph::Node &msfg_node = nodes[msfg_src_node];
@@ -52,14 +52,14 @@ MultiStringFactorGraph::add(const FactorGraph &text)
             }
 
             // Not found, check if node added but no arc yet
-            if (msfg_target_node < 0)
+            if (msfg_target_node == 0)
             if (created_nodes.find(fg_target_node) != created_nodes.end()) {
                 msfg_target_node = created_nodes[fg_target_node];
                 create_arc(msfg_src_node, msfg_target_node);
             }
 
             // Create node and arc
-            if (msfg_target_node < 0) {
+            if (msfg_target_node == 0) {
                 nodes.push_back(Node(target_node_factor));
                 msfg_target_node = nodes.size()-1;
                 created_nodes[fg_target_node] = msfg_target_node;
@@ -74,7 +74,7 @@ MultiStringFactorGraph::add(const FactorGraph &text)
     nodes.push_back(Node(start_end_symbol));
     created_nodes[text.nodes.size()-1] = nodes.size()-1;
     for (auto it = connect_to_end_node.begin(); it != connect_to_end_node.end(); ++it)
-        create_arc(*it, nodes.size()-1);
+        create_arc((*it), (nodes.size()-1));
     string_end_nodes[text.text] = nodes.size()-1;
 }
 
@@ -84,13 +84,13 @@ MultiStringFactorGraph::num_paths(const std::string &text) const
 {
     if (nodes.size() == 0) return 0;
     if (string_end_nodes.find(text) == string_end_nodes.end()) return 0;
-    int end_node = string_end_nodes.at(text);
-    map<int, int> path_counts; path_counts[end_node] = 1;
-    set<int> nodes_to_process; nodes_to_process.insert(end_node);
+    msfg_node_idx_t end_node = string_end_nodes.at(text);
+    map<msfg_node_idx_t, int> path_counts; path_counts[end_node] = 1;
+    set<msfg_node_idx_t> nodes_to_process; nodes_to_process.insert(end_node);
 
     while(nodes_to_process.size() > 0) {
 
-        int i = *(nodes_to_process.rbegin());
+        msfg_node_idx_t i = *(nodes_to_process.rbegin());
 
         const Node &node = nodes[i];
         for (auto arc = node.incoming.begin(); arc != node.incoming.end(); ++arc) {
