8c8c33ff2a2aa05c541a2684f8378fe94b29dcc9
Matti Varjokallio <matti.varjokallio@iki.fi>
2013-06-08 23:08:03 +0300

    Better factor data types for FG.

--- defs.hh
+++ defs.hh
@@ -6,8 +6,8 @@
 typedef double flt_type;
 typedef unsigned short int fg_node_idx_t;
 typedef unsigned int msfg_node_idx_t;
-typedef char factor_pos_t;
-typedef char factor_len_t;
+typedef unsigned char factor_pos_t;
+typedef unsigned char factor_len_t;
 
 static flt_type SMALL_LP = -100.0;
 static flt_type MIN_FLOAT = -std::numeric_limits<flt_type>::max();
