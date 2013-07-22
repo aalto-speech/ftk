288c0c001053cee18b514435443f9e975296b58d
Matti Varjokallio <matti.varjokallio@aalto.fi>
2013-07-22 10:17:04 +0300

    More test cases.

--- sstest.cc
+++ sstest.cc
@@ -49,12 +49,67 @@ void sstest :: StringSetTest1 (void)
     CPPUNIT_ASSERT ( !ss.includes("h") );
 }
 
-
+// Add multiple strings
 void sstest :: StringSetTest2 (void)
 {
+    StringSet<flt_type> ss;
+    ss.add("hei", -1.0);
+    ss.add("heippa", -2.0);
+    ss.add("heh", -3.0);
+    ss.add("hassua", -4.0);
+    ss.add("hassu", -5.0);
+
+    CPPUNIT_ASSERT ( ss.includes("hei") );
+    CPPUNIT_ASSERT ( ss.includes("heippa") );
+    CPPUNIT_ASSERT ( ss.includes("heh") );
+    CPPUNIT_ASSERT ( ss.includes("hassua") );
+    CPPUNIT_ASSERT ( ss.includes("hassu") );
+    CPPUNIT_ASSERT ( !ss.includes("h") );
+    CPPUNIT_ASSERT ( !ss.includes("he") );
+    CPPUNIT_ASSERT ( !ss.includes("heip") );
+
+    CPPUNIT_ASSERT_EQUAL ( -1.0, ss.get_score("hei") );
+    CPPUNIT_ASSERT_EQUAL ( -2.0, ss.get_score("heippa") );
+    CPPUNIT_ASSERT_EQUAL ( -3.0, ss.get_score("heh") );
+    CPPUNIT_ASSERT_EQUAL ( -4.0, ss.get_score("hassua") );
+    CPPUNIT_ASSERT_EQUAL ( -5.0, ss.get_score("hassu") );
+
+    bool throws = false;
+    try {
+        ss.get_score("he");
+    }
+    catch (string e) {
+        throws = true;
+    }
+    CPPUNIT_ASSERT( throws );
 }
 
-
+// Add and remove
 void sstest :: StringSetTest3 (void)
 {
+    StringSet<flt_type> ss;
+    ss.add("hei", -1.0);
+    ss.add("heippa", -2.0);
+    ss.add("heh", -3.0);
+
+    CPPUNIT_ASSERT ( ss.includes("hei") );
+    CPPUNIT_ASSERT ( ss.includes("heippa") );
+    CPPUNIT_ASSERT ( ss.includes("heh") );
+
+    ss.remove("hei");
+    CPPUNIT_ASSERT ( ss.includes("heh") );
+    CPPUNIT_ASSERT ( ss.includes("heippa") );
+    CPPUNIT_ASSERT ( !ss.includes("hei") );
+
+    CPPUNIT_ASSERT_EQUAL ( -2.0, ss.get_score("heippa") );
+    CPPUNIT_ASSERT_EQUAL ( -3.0, ss.get_score("heh") );
+
+    bool throws = false;
+    try {
+        ss.get_score("hei");
+    }
+    catch (string e) {
+        throws = true;
+    }
+    CPPUNIT_ASSERT( throws );
 }
