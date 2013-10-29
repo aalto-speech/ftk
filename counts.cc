d67786d4f4829b616a601cb9f92301fa665e571e
Matti Varjokallio <matti.varjokallio@iki.fi>
2013-10-29 23:50:16 +0200

    Rewrote tool to use Aalto conf.

--- counts.cc
+++ counts.cc
@@ -8,9 +8,8 @@
 #include <string>
 #include <vector>
 
-#include <popt.h>
-
 #include "defs.hh"
+#include "conf.hh"
 #include "io.hh"
 #include "StringSet.hh"
 #include "FactorGraph.hh"
@@ -23,88 +22,42 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    char *vocab_fname = NULL;
-    char *trans_fname = NULL;
-    string in_fname;
-    string out_fname_1;
-    string out_fname_2;
-    int maxlen;
-    map<string, flt_type> vocab;
-    StringSet *ss_vocab = NULL;
-    transitions_t transitions;
-    flt_type one_char_min_lp = -25.0;
-    bool enable_forward_backward = false;
-    bool weights = false;
+    conf::Config config;
+    config("usage: segposts [OPTION...] INPUT COUNTS1 COUNTS2\n")
+      ('h', "help", "", "", "display help")
+      ('v', "vocabulary=FILE", "arg", "", "Unigram model file")
+      ('t', "transitions=FILE", "arg", "", "Bigram model file")
+      ('f', "forward-backward", "", "", "Use Forward-backward segmentation instead of Viterbi")
+      ('w', "weights", "", "", "Training examples are weighted");
+    config.default_parse(argc, argv);
+    if (config.arguments.size() != 3) config.print_help(stderr, 1);
 
-    poptContext pc;
-    struct poptOption po[] = {
-        {"vocabulary", 'v', POPT_ARG_STRING, &vocab_fname, 11001, NULL, "Vocabulary file name"},
-        {"transitions", 't', POPT_ARG_STRING, &trans_fname, 11002, NULL, "Transition file name"},
-        {"forward_backward", 'f', POPT_ARG_NONE, &enable_forward_backward, 11007, "Use Forward-backward segmentation instead of Viterbi", NULL},
-        {"weights", 'w', POPT_ARG_NONE, &weights, 11007, "Training examples are weighted", NULL},
-        POPT_AUTOHELP
-        {NULL}
-    };
-
-    pc = poptGetContext(NULL, argc, (const char **)argv, po, 0);
-    poptSetOtherOptionHelp(pc, "INPUT COUNTS1 COUNTS2");
-
-    int val;
-    while ((val = poptGetNextOpt(pc)) >= 0)
-        continue;
-
-    // poptGetNextOpt returns -1 when the final argument has been parsed
-    // otherwise an error occured
-    if (val != -1) {
-        switch (val) {
-        case POPT_ERROR_NOARG:
-            cerr << "Argument missing for an option" << endl;
-            exit(1);
-        case POPT_ERROR_BADOPT:
-            cerr << "Option's argument could not be parsed" << endl;
-            exit(1);
-        case POPT_ERROR_BADNUMBER:
-        case POPT_ERROR_OVERFLOW:
-            cerr << "Option could not be converted to number" << endl;
-            exit(1);
-        default:
-            cerr << "Unknown error in option processing" << endl;
-            exit(1);
-        }
-    }
-
-    if (poptPeekArg(pc) != NULL)
-        in_fname.assign((char*)poptGetArg(pc));
-    else {
-        cerr << "Input file not set" << endl;
-        exit(1);
-    }
-
-    if (poptPeekArg(pc) != NULL)
-        out_fname_1.assign((char*)poptGetArg(pc));
-    else {
-        cerr << "1-gram count file not set" << endl;
-        exit(1);
-    }
-
-    if (poptPeekArg(pc) != NULL)
-        out_fname_2.assign((char*)poptGetArg(pc));
-    else {
-        cerr << "2-gram count file not set" << endl;
-        exit(1);
-    }
-
-    if (vocab_fname == NULL && trans_fname == NULL) {
+    if (!config["vocabulary"].specified && !config["transitions"].specified) {
         cerr << "Please define vocabulary or transitions" << endl;
         exit(0);
     }
 
-    if (vocab_fname != NULL && trans_fname != NULL) {
-        cerr << "Don't define both vocabulary and transitions" << endl;
+    if (config["vocabulary"].specified && config["transitions"].specified) {
+        cerr << "Please don't define both vocabulary and transitions" << endl;
         exit(0);
     }
 
-    if (vocab_fname != NULL) {
+    string vocab_fname;
+    string trans_fname;
+    int maxlen;
+    map<string, flt_type> vocab;
+    StringSet *ss_vocab = NULL;
+    transitions_t transitions;
+    bool unigram = true;
+    flt_type one_char_min_lp = -25.0;
+    string in_fname = config.arguments[0];
+    string out_fname_1 = config.arguments[1];
+    string out_fname_2 = config.arguments[2];
+    bool enable_forward_backward = config["forward-backward"].specified;
+    bool weights = config["weights"].specified;
+
+    if (config["vocabulary"].specified) {
+        vocab_fname = config["vocabulary"].get_str();
         cerr << "Reading vocabulary " << vocab_fname << endl;
         int retval = Unigrams::read_vocab(vocab_fname, vocab, maxlen);
         vocab[start_end_symbol] = 0.0;
@@ -117,7 +70,9 @@ int main(int argc, char* argv[]) {
         ss_vocab = new StringSet(vocab);
     }
 
-    if (trans_fname != NULL) {
+    if (config["transitions"].specified) {
+        unigram = false;
+        trans_fname = config["vocabulary"].get_str();
         cerr << "Reading transitions " << trans_fname << endl;
         int retval = Bigrams::read_transitions(transitions, trans_fname);
         Bigrams::trans_to_vocab(transitions, vocab);
@@ -170,8 +125,7 @@ int main(int argc, char* argv[]) {
 
         transitions_t curr_stats;
 
-        // Unigram model
-        if (vocab_fname != NULL)
+        if (unigram)
             if (enable_forward_backward) {
                 FactorGraph fg(line, start_end_symbol, *ss_vocab);
                 forward_backward(vocab, fg, curr_stats);
@@ -179,7 +133,6 @@ int main(int argc, char* argv[]) {
             else {
                 viterbi(vocab, line, curr_stats, start_end_symbol);
             }
-        // Bigram model
         else {
             FactorGraph fg(line, start_end_symbol, *ss_vocab);
             if (enable_forward_backward)
