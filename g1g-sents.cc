a5d9ba39b25abfa3cbe0474cdf44ccfe77df3231
Matti Varjokallio <matti.varjokallio@iki.fi>
2013-10-30 00:13:13 +0200

    Rewrote tool to use Aalto conf.

--- g1g-sents.cc
+++ g1g-sents.cc
@@ -7,9 +7,8 @@
 #include <string>
 #include <vector>
 
-#include <popt.h>
-
 #include "defs.hh"
+#include "conf.hh"
 #include "StringSet.hh"
 #include "FactorEncoder.hh"
 #include "Unigrams.hh"
@@ -29,81 +28,34 @@ void assert_single_chars(map<string, flt_type> &vocab,
 
 int main(int argc, char* argv[]) {
 
-    float cutoff_value = 0.0;
-    int n_candidates_per_iter = 5000;
-    int max_removals_per_iter = 5000;
-    int min_removals_per_iter = 0;
-    int target_vocab_size = 50000;
-    bool enable_forward_backward = false;
+    conf::Config config;
+    config("usage: g1g-sents [OPTION...] CORPUS VOCAB_INIT VOCAB_OUTNAME\n")
+      ('h', "help", "", "", "display help")
+      ('u', "cutoff=INT", "arg", "0", "Cutoff value for each iteration")
+      ('c', "candidates=INT", "arg", "5000", "Number of candidate strings to try to remove per iteration")
+      ('r', "removals=INT", "arg", "500", "Number of removals per iteration")
+      ('v', "vocab-size=INT", "arg must", "", "Target vocabulary size (stopping criterion)")
+      ('f', "forward-backward", "", "", "Use Forward-backward segmentation instead of Viterbi");
+    config.default_parse(argc, argv);
+    if (config.arguments.size() != 3) config.print_help(stderr, 1);
+
     flt_type one_char_min_lp = -25.0;
-    string vocab_fname;
-    string corpus_fname;
-
-    // Popt documentation:
-    // http://linux.die.net/man/3/popt
-    // http://privatemisc.blogspot.fi/2012/12/popt-basic-example.html
-    poptContext pc;
-    struct poptOption po[] = {
-        {"cutoff", 'u', POPT_ARG_FLOAT, &cutoff_value, 11001, NULL, "Cutoff value for each iteration"},
-        {"candidates", 'c', POPT_ARG_INT, &n_candidates_per_iter, 11002, NULL, "Number of candidate subwords to try to remove per iteration"},
-        {"max_removals", 'a', POPT_ARG_INT, &max_removals_per_iter, 11003, NULL, "Maximum number of removals per iteration"},
-        {"min_removals", 'i', POPT_ARG_INT, &min_removals_per_iter, 11004, NULL, "Minimum number of removals per iteration (stopping criterion)"},
-        {"vocab_size", 'g', POPT_ARG_INT, &target_vocab_size, 11007, NULL, "Target vocabulary size (stopping criterion)"},
-        {"forward_backward", 'f', POPT_ARG_NONE, &enable_forward_backward, 11007, "Use Forward-backward segmentation instead of Viterbi", NULL},
-        POPT_AUTOHELP
-        {NULL}
-    };
-
-    pc = poptGetContext(NULL, argc, (const char **)argv, po, 0);
-    poptSetOtherOptionHelp(pc, "[INITIAL VOCABULARY] [TRAINING CORPUS]");
-
-    int val;
-    while ((val = poptGetNextOpt(pc)) >= 0)
-        continue;
-
-    // poptGetNextOpt returns -1 when the final argument has been parsed
-    // otherwise an error occured
-    if (val != -1) {
-        switch (val) {
-        case POPT_ERROR_NOARG:
-            cerr << "Argument missing for an option" << endl;
-            exit(1);
-        case POPT_ERROR_BADOPT:
-            cerr << "Option's argument could not be parsed" << endl;
-            exit(1);
-        case POPT_ERROR_BADNUMBER:
-        case POPT_ERROR_OVERFLOW:
-            cerr << "Option could not be converted to number" << endl;
-            exit(1);
-        default:
-            cerr << "Unknown error in option processing" << endl;
-            exit(1);
-        }
-    }
-
-    // Handle ARG part of command line
-    if (poptPeekArg(pc) != NULL)
-        vocab_fname.assign((char*)poptGetArg(pc));
-    else {
-        cerr << "Initial vocabulary file not set" << endl;
-        exit(1);
-    }
-
-    if (poptPeekArg(pc) != NULL)
-        corpus_fname.assign((char*)poptGetArg(pc));
-    else {
-        cerr << "Training corpus not set" << endl;
-        exit(1);
-    }
+    string corpus_fname = config.arguments[0];
+    string vocab_fname = config.arguments[1];
+    string out_vocab_fname = config.arguments[2];
+    float cutoff_value = config["cutoff"].get_float();
+    int n_candidates_per_iter = config["candidates"].get_int();
+    int removals_per_iter = config["removals"].get_int();
+    int target_vocab_size = config["vocab-size"].get_int();
+    bool enable_forward_backward = config["forward-backward"].specified;
 
     cerr << "parameters, initial vocabulary: " << vocab_fname << endl;
     cerr << "parameters, training corpus: " << corpus_fname << endl;
     cerr << "parameters, cutoff: " << setprecision(15) << cutoff_value << endl;
     cerr << "parameters, candidates per iteration: " << n_candidates_per_iter << endl;
-    cerr << "parameters, removals per iteration: " << max_removals_per_iter << endl;
+    cerr << "parameters, removals per iteration: " << removals_per_iter << endl;
     cerr << "below 100k.." << endl;
     cerr << "parameters, removals per iteration: 500" << endl;
-    cerr << "parameters, min removals per iteration: " << min_removals_per_iter << endl;
     cerr << "parameters, target vocab size: " << target_vocab_size << endl;
     cerr << "parameters, use forward-backward: " << enable_forward_backward << endl;
 
@@ -198,7 +150,7 @@ int main(int argc, char* argv[]) {
                 Unigrams::write_vocab(vocabfname.str(), vocab);
             }
 
-            if (n_removals >= max_removals_per_iter) break;
+            if (n_removals >= removals_per_iter) break;
             if (vocab.size() <= target_vocab_size) break;
         }
 
@@ -220,11 +172,6 @@ int main(int argc, char* argv[]) {
 
         itern++;
 
-        if (n_removals < min_removals_per_iter) {
-            cerr << "stopping by min_removals_per_iter." << endl;
-            break;
-        }
-
         if (vocab.size() <= target_vocab_size) {
             cerr << "stopping by min_vocab_size." << endl;
             break;
@@ -235,4 +182,3 @@ int main(int argc, char* argv[]) {
 
     exit(1);
 }
-
