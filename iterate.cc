3fd45bc11cad31a01ccadbf9d72b6cafb56c9831
Matti Varjokallio <matti.varjokallio@iki.fi>
2013-10-29 14:14:18 +0200

    Rewrote tool to use Aalto conf.

--- iterate.cc
+++ iterate.cc
@@ -8,9 +8,8 @@
 #include <string>
 #include <vector>
 
-#include <popt.h>
-
 #include "defs.hh"
+#include "conf.hh"
 #include "StringSet.hh"
 #include "FactorEncoder.hh"
 #include "Unigrams.hh"
@@ -30,75 +29,24 @@ void assert_single_chars(map<string, flt_type> &vocab,
 
 int main(int argc, char* argv[]) {
 
-    bool enable_forward_backward = false;
-    int num_iterations = 5;
     flt_type one_char_min_lp = -25.0;
-    string vocab_in_fname;
-    string vocab_out_fname;
-    string wordlist_fname;
 
-    // Popt documentation:
-    // http://linux.die.net/man/3/popt
-    // http://privatemisc.blogspot.fi/2012/12/popt-basic-example.html
-    poptContext pc;
-    struct poptOption po[] = {
-        {"iterations", 'i', POPT_ARG_INT, &num_iterations, 11003, NULL, "Number of iterations"},
-        {"forward_backward", 'f', POPT_ARG_NONE, &enable_forward_backward, 11007, "Use Forward-backward segmentation instead of Viterbi", NULL},
-        POPT_AUTOHELP
-        {NULL}
-    };
+    conf::Config config;
+    config("usage: iterate [OPTION...] WORDLIST IN_VOCAB OUT_VOCAB\n")
+      ('h', "help", "", "", "display help")
+      ('i', "iterations=INT", "arg", "5", "Number of iterations")
+      ('f', "forward-backward", "", "", "Use Forward-backward segmentation instead of Viterbi");
+    config.default_parse(argc, argv);
+    if (config.arguments.size() != 3) config.print_help(stderr, 1);
 
-    pc = poptGetContext(NULL, argc, (const char **)argv, po, 0);
-    poptSetOtherOptionHelp(pc, "[INITIAL_VOCABULARY] [WORDLIST] [FINAL_VOCAB]");
+    int num_iterations = config["iterations"].get_int();
+    bool enable_forward_backward = config["forward-backward"].specified;
+    string wordlist_fname = config.arguments[0];
+    string vocab_in_fname = config.arguments[1];
+    string vocab_out_fname = config.arguments[2];
 
-    int val;
-    while ((val = poptGetNextOpt(pc)) >= 0)
-        continue;
-
-    // poptGetNextOpt returns -1 when the final argument has been parsed
-    // otherwise an error occured
-    if (val != -1) {
-        switch (val) {
-        case POPT_ERROR_NOARG:
-            cerr << "Argument missing for an option" << endl;
-            exit(1);
-        case POPT_ERROR_BADOPT:
-            cerr << "Option's argument could not be parsed" << endl;
-            exit(1);
-        case POPT_ERROR_BADNUMBER:
-        case POPT_ERROR_OVERFLOW:
-            cerr << "Option could not be converted to number" << endl;
-            exit(1);
-        default:
-            cerr << "Unknown error in option processing" << endl;
-            exit(1);
-        }
-    }
-
-    // Handle ARG part of command line
-    if (poptPeekArg(pc) != NULL)
-        vocab_in_fname.assign((char*)poptGetArg(pc));
-    else {
-        cerr << "Initial vocabulary file not set" << endl;
-        exit(1);
-    }
-
-    if (poptPeekArg(pc) != NULL)
-        wordlist_fname.assign((char*)poptGetArg(pc));
-    else {
-        cerr << "Wordlist file not set" << endl;
-        exit(1);
-    }
-
-    if (poptPeekArg(pc) != NULL)
-        vocab_out_fname.assign((char*)poptGetArg(pc));
-    else {
-        cerr << "Output vocabulary file not set" << endl;
-        exit(1);
-    }
-
-    cerr << "parameters, initial vocabulary: " << vocab_in_fname << endl;
     cerr << "parameters, wordlist: " << wordlist_fname << endl;
+    cerr << "parameters, initial vocabulary: " << vocab_in_fname << endl;
     cerr << "parameters, final vocabulary: " << vocab_out_fname << endl;
     cerr << "parameters, use forward-backward: " << enable_forward_backward << endl;
     cerr << "parameters, number of iterations: " << num_iterations << endl;
@@ -108,7 +56,6 @@ int main(int argc, char* argv[]) {
     map<string, flt_type> vocab;
     map<string, flt_type> freqs;
     map<string, flt_type> words;
-    map<string, flt_type> unit_words;
 
     cerr << "Reading vocabulary " << vocab_in_fname << endl;
     int retval = Unigrams::read_vocab(vocab_in_fname, vocab, maxlen);
@@ -155,4 +102,3 @@ int main(int argc, char* argv[]) {
 
     exit(1);
 }
-
