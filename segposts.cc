06f922ff812d2671539b341234af90d6a05f2d1a
Matti Varjokallio <matti.varjokallio@iki.fi>
2013-10-29 22:51:38 +0200

    Rewrote tool to use Aalto conf.

--- segposts.cc
+++ segposts.cc
@@ -8,9 +8,8 @@
 #include <string>
 #include <vector>
 
-#include <popt.h>
-
 #include "defs.hh"
+#include "conf.hh"
 #include "io.hh"
 #include "StringSet.hh"
 #include "FactorGraph.hh"
@@ -23,75 +22,37 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
 
-    char *vocab_fname;
-    char *trans_fname;
-    string in_fname;
-    string out_fname;
+    string vocab_fname;
+    string trans_fname;
     int maxlen;
     map<string, flt_type> vocab;
     StringSet *ss_vocab = NULL;
     transitions_t transitions;
+    bool unigram = true;
 
-    poptContext pc;
-    struct poptOption po[] = {
-        {"vocabulary", 'v', POPT_ARG_STRING, &vocab_fname, 11001, NULL, "Vocabulary file name"},
-        {"transitions", 't', POPT_ARG_STRING, &trans_fname, 11002, NULL, "Transition file name"},
-        POPT_AUTOHELP
-        {NULL}
-    };
+    conf::Config config;
+    config("usage: segposts [OPTION...] INPUT SEGPROBS_OUTPUT\n")
+      ('h', "help", "", "", "display help")
+      ('v', "vocabulary=FILE", "arg", "", "Unigram model file")
+      ('v', "transitions=FILE", "arg", "", "Bigram model file");
+    config.default_parse(argc, argv);
+    if (config.arguments.size() != 2) config.print_help(stderr, 1);
 
-    pc = poptGetContext(NULL, argc, (const char **)argv, po, 0);
-    poptSetOtherOptionHelp(pc, "INPUT SEGPROBS_OUTPUT");
+    string in_fname = config.arguments[0];
+    string out_fname = config.arguments[1];
 
-    int val;
-    while ((val = poptGetNextOpt(pc)) >= 0)
-        continue;
-
-    // poptGetNextOpt returns -1 when the final argument has been parsed
-    // otherwise an error occured
-    if (val != -1) {
-        switch (val) {
-        case POPT_ERROR_NOARG:
-            cerr << "Argument missing for an option" << endl;
-            exit(1);
-        case POPT_ERROR_BADOPT:
-            cerr << "Option's argument could not be parsed" << endl;
-            exit(1);
-        case POPT_ERROR_BADNUMBER:
-        case POPT_ERROR_OVERFLOW:
-            cerr << "Option could not be converted to number" << endl;
-            exit(1);
-        default:
-            cerr << "Unknown error in option processing" << endl;
-            exit(1);
-        }
-    }
-
-    if (poptPeekArg(pc) != NULL)
-        in_fname.assign((char*)poptGetArg(pc));
-    else {
-        cerr << "Input file not set" << endl;
-        exit(1);
-    }
-
-    if (poptPeekArg(pc) != NULL)
-        out_fname.assign((char*)poptGetArg(pc));
-    else {
-        cerr << "Output file not set" << endl;
-        exit(1);
-    }
-
-    if (vocab_fname == NULL && trans_fname == NULL) {
+    if (!config["vocabulary"].specified && !config["transitions"].specified) {
         cerr << "Please define vocabulary or transitions" << endl;
         exit(0);
     }
 
-    if (vocab_fname != NULL && trans_fname != NULL) {
-        cerr << "Don't define both vocabulary and transitions" << endl;
+    if (config["vocabulary"].specified && config["transitions"].specified) {
+        cerr << "Please don't define both vocabulary and transitions" << endl;
         exit(0);
     }
 
-    if (vocab_fname != NULL) {
+    if (config["vocabulary"].specified) {
+        vocab_fname = config["vocabulary"].get_str();
         cerr << "Reading vocabulary " << vocab_fname << endl;
         int retval = Unigrams::read_vocab(vocab_fname, vocab, maxlen);
         if (retval < 0) {
@@ -103,7 +64,9 @@ int main(int argc, char* argv[]) {
         ss_vocab = new StringSet(vocab);
     }
 
-    if (trans_fname != NULL) {
+    if (config["transitions"].specified) {
+        unigram = false;
+        vocab_fname = config["transitions"].get_str();
         cerr << "Reading transitions " << trans_fname << endl;
         int retval = Bigrams::read_transitions(transitions, trans_fname);
         Bigrams::trans_to_vocab(transitions, vocab);
@@ -127,8 +90,8 @@ int main(int argc, char* argv[]) {
         exit(0);
     }
 
-    char linebuffer[8192];
-    while (fgets(linebuffer, 8192 , infile.file) != NULL) {
+    char linebuffer[MAX_LINE_LEN];
+    while (fgets(linebuffer, MAX_LINE_LEN, infile.file) != NULL) {
 
         linebuffer[strlen(linebuffer)-1] = '\0';
         string line(linebuffer);
@@ -136,12 +99,8 @@ int main(int argc, char* argv[]) {
         transitions_t bg_stats;
         vector<flt_type> post_scores;
 
-        // Unigram
-        if (vocab_fname != NULL) {
+        if (unigram)
             forward_backward(*ss_vocab, line, ug_stats, post_scores);
-        }
-
-        // Bigram
         else {
             FactorGraph fg(line, start_end_symbol, *ss_vocab);
             forward_backward(transitions, fg, bg_stats, post_scores);
