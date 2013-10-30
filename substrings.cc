c05a7b37852647570c7ecf68764e6c97eb7212d8
Matti Varjokallio <matti.varjokallio@aalto.fi>
2013-10-30 11:33:10 +0200

    Rewrote tool to use Aalto conf.

--- substrings.cc
+++ substrings.cc
@@ -5,6 +5,8 @@
 #include <sstream>
 #include <vector>
 
+#include "conf.hh"
+
 using namespace std;
 
 
@@ -16,8 +18,10 @@ int get_substrings(const string &infname,
                    map<string,int> &substrs) {
 
     ifstream infile(infname);
-    if (!infile)
-        return -1;
+    if (!infile) {
+        cerr << "Something went wrong opening the input file." << endl;
+        exit(0);
+    }
 
     int count;
     string line, word;
@@ -37,18 +41,37 @@ int get_substrings(const string &infname,
 }
 
 
-int main(int argc, char* argv[]) {
-
-    if (argc < 2) {
-        cerr << "usage: " << argv[0] << " <wordlist> <maxlen>" << endl;
+void write_substrings(const string &outfname,
+                      vector<pair<string, int> > &sorted_substrs)
+{
+    ofstream outfile(outfname);
+    if (!outfile) {
+        cerr << "Something went wrong opening the output file." << endl;
         exit(0);
     }
 
+    for (auto it = sorted_substrs.cbegin(); it != sorted_substrs.cend(); ++it)
+        outfile << it->second << "\t" << it->first << endl;
+
+    outfile.close();
+}
+
+
+int main(int argc, char* argv[]) {
+
+    conf::Config config;
+    config("usage: fe [OPTION...] INPUT OUTPUT\n")
+      ('h', "help", "", "", "display help")
+      ('l', "max-length=INT", "arg must", "100", "Maximum length of the substrings");
+    config.default_parse(argc, argv);
+    if (config.arguments.size() != 2) config.print_help(stderr, 1);
+
+    string infname = config.arguments[0];
+    string outfname = config.arguments[1];
+    int maxlen = config["max-length"].get_int();
+
     map<string, int> substrs;
-    string fname(argv[1]);
-    int maxlen = 100;
-    if (argc > 2) maxlen = atoi(argv[2]);
-    int substr_count = get_substrings(fname, maxlen, substrs);
+    int substr_count = get_substrings(infname, maxlen, substrs);
 
     vector<pair<string, int> > sorted_substrs;
     for (auto it = substrs.cbegin(); it != substrs.cend(); ++it)
@@ -56,8 +79,7 @@ int main(int argc, char* argv[]) {
     sort(sorted_substrs.begin(), sorted_substrs.end(), desc_sort);
 
     cerr << substr_count << " substrings in total up to length of " << maxlen << endl;
-    for (auto it = sorted_substrs.cbegin(); it != sorted_substrs.cend(); ++it)
-        cout << it->second << "\t" << it->first << endl;
+    write_substrings(outfname, sorted_substrs);
 
     exit(1);
 }
