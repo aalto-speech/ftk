44d56c7a5f4cb7d2d2e36c99a422966e7f5af5ce
Matti Varjokallio <matti.varjokallio@aalto.fi>
2014-04-15 18:03:44 +0300

    Rewritten script with utf-8 support.

--- wlist2srilm.py
+++ wlist2srilm.py
@@ -1,17 +1,44 @@
 #!/usr/bin/python
 
+import os
 import sys
+import codecs
+from optparse import OptionParser
 
 
-for line in sys.stdin:
-    line = line.strip()
-    vals = line.split()
-    letters = list(vals[1])
+if __name__ == "__main__":
 
-    sys.stdout.write(vals[0])
-    sys.stdout.write(" <s>")
-    for letter in letters:
-        sys.stdout.write(" ")
-        sys.stdout.write(letter)
-    sys.stdout.write(" </s>\n")
+    usage = "usage: %prog [options] [input] [output]"
+    description = "Convert list of word counts for letter n-gram training."
+    parser = OptionParser(usage=usage, description=description)
+    parser.add_option('--utf-8', dest='utf8', action='store_true', default=False,
+                      help='Input is in Utf-8 format')
+
+    (options, args) = parser.parse_args()
+    infile = None
+    outfile = None
+
+    infile = sys.stdin
+    outfile = sys.stdout
+    if len(args):
+        if not len(args) == 2:
+            print >>sys.stderr, "Please define either no input/output files (stdin and stdout will be used) or both."
+            sys.exit(0)
+        infile = open(args[0])
+        outfile = open(args[1], 'w')
+
+    if options.utf8:
+        infile = codecs.getreader('utf-8')(infile)
+        outfile = codecs.getwriter('utf-8')(outfile)
+
+    for line in infile:
+        line = line.strip()
+        vals = line.split()
+        letters = list(vals[1])
+
+        outfile.write("%s <s>" % vals[0])
+        for letter in letters:
+            outfile.write(" ")
+            outfile.write(letter)
+        outfile.write(" </s>%s" % os.linesep)
 
