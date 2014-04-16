2a0a4c36e1aa7af6f5fa279a5c5a4ce5cfdabf19
Matti Varjokallio <matti.varjokallio@aalto.fi>
2014-04-16 13:40:18 +0300

    Utf-8 switch for the executable.

--- ll.cc
+++ ll.cc
@@ -21,17 +21,20 @@ int main(int argc, char* argv[]) {
     conf::Config config;
     config("usage: ll [OPTION...] WORDLIST VOCABULARY\n")
       ('h', "help", "", "", "display help")
-      ('f', "forward-backward", "", "", "Use Forward-backward segmentation instead of Viterbi");
+      ('f', "forward-backward", "", "", "Use Forward-backward segmentation instead of Viterbi")
+      ('8', "utf-8", "", "", "Utf-8 character encoding in use");
     config.default_parse(argc, argv);
     if (config.arguments.size() != 2) config.print_help(stderr, 1);
 
     bool enable_forward_backward = config["forward-backward"].specified;
+    bool utf8_encoding = config["utf-8"].specified;
     string wordlist_fname = config.arguments[0];
     string vocab_in_fname = config.arguments[1];
 
     cerr << "parameters, wordlist: " << wordlist_fname << endl;
     cerr << "parameters, vocabulary: " << vocab_in_fname << endl;
     cerr << "parameters, use forward-backward: " << enable_forward_backward << endl;
+    cerr << "parameters, utf-8 encoding: " << utf8_encoding << endl;
 
     int maxlen, word_maxlen;
     map<string, flt_type> vocab;
@@ -61,6 +64,7 @@ int main(int argc, char* argv[]) {
         gg.set_segmentation_method(forward_backward);
     else
         gg.set_segmentation_method(viterbi);
+    gg.set_utf8(utf8_encoding);
 
     flt_type ll = gg.resegment_words(words, vocab, freqs);
     cerr << "cost: " << ll << endl;
