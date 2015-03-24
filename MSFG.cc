4f073da7df8a39680de5556df847e2f98fcf0ae8
Matti Varjokallio <matti.varjokallio@aalto.fi>
2015-03-24 14:17:53 +0200

    Write factors out correctly.

--- MSFG.cc
+++ MSFG.cc
@@ -259,8 +259,8 @@ MultiStringFactorGraph::write(const std::string &filename) const
     vector<Arc*> arcs; collect_arcs(arcs);
 
     outfile << nodes.size() << " " << arcs.size() << " " << string_end_nodes.size() << endl;
-    for (unsigned int i=0; i<nodes.size(); i++)
-        outfile << "n " << i << " " << nodes[i].factor << endl;
+    for (msfg_node_idx_t i=0; i<nodes.size(); i++)
+        outfile << "n " << i << " " << get_factor(i) << endl;
     for (auto it = arcs.begin(); it != arcs.end(); ++it)
         outfile << "a " << (**it).source_node << " " << (**it).target_node << endl;
     for (auto it = string_end_nodes.cbegin(); it != string_end_nodes.cend(); ++it)
