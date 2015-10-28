dcadc0a74f16fb3f3fb6fb4382e9db0be1329e30
Matti Varjokallio <matti.varjokallio@aalto.fi>
2015-10-28 10:11:07 +0200

    Improved print options.

--- segtext.cc
+++ segtext.cc
@@ -24,12 +24,14 @@ int main(int argc, char* argv[]) {
       ('h', "help", "", "", "display help")
       ('v', "vocabulary=FILE", "arg", "", "Unigram model file")
       ('t', "transitions=FILE", "arg", "", "Bigram model file")
-      ('s', "sentence-mode", "", "", "Prints <s>, </s>, <w> symbols")
+      ('s', "sentence-mode", "", "", "Prints <s> and </s> symbols")
+      ('b', "word-boundaries", "", "", "Prints <w> symbols between words")
       ('8', "utf-8", "", "", "Utf-8 character encoding in use");
     config.default_parse(argc, argv);
     if (config.arguments.size() != 2) config.print_help(stderr, 1);
 
     bool sentence_mode = config["sentence-mode"].specified;
+    bool word_boundaries = config["word-boundaries"].specified;
     bool utf8_encoding = config["utf-8"].specified;
     string in_fname = config.arguments[0];
     string out_fname = config.arguments[1];
@@ -43,6 +45,9 @@ int main(int argc, char* argv[]) {
         cerr << "Please don't define both vocabulary and transitions" << endl;
         exit(0);
     }
+
+    if (!sentence_mode && word_boundaries)
+        cerr << "Word boundaries printed only in sentence mode." << endl;
 
     if (config["vocabulary"].specified) {
         vocab_fname = config["vocabulary"].get_str();
@@ -102,14 +107,16 @@ int main(int argc, char* argv[]) {
 
         // Print out the best path
         if (sentence_mode) {
-            outfile << "<s> <w>";
+            if (word_boundaries) outfile << "<s> <w>";
+            else outfile << "<s>";
             for (unsigned int i=0; i<best_path.size(); i++) {
-                if (best_path[i] == " ")
+                if (best_path[i] == " " && word_boundaries)
                     outfile << " <w>";
                 else
                     outfile << " " << best_path[i];
             }
-            outfile << " <w> </s>\n";
+            if (word_boundaries) outfile << " <w> </s>\n";
+            else outfile << " </s>\n";
         }
         else {
             for (unsigned int i=0; i<best_path.size(); i++) {
